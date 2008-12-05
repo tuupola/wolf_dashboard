022bcb8f8b27478a4b3ac19bfd7ebde505c2aaee
Mika Tuupola <tuupola@appelsiini.net>
2008-12-05 12:59:28 +0200

    Log layout and snippet events.
    NOTE! These currently only work with MIT fork of Frog. Patch to official AGPL licensed Frog most likely to follow.
    
    http://github.com/tuupola/monolith

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ Plugin::setInfos(array(
     'description' => 'Keep up to date what is happening with your site.', 
     'version'     => '0.1.0', 
     'license'     => 'MIT',
-    'require_frog_version' => '0.9.4',
+    'require_frog_version' => '0.9.3',
     'update_url'  => 'http://www.appelsiini.net/download/frog-plugins.xml',
     'website'     => 'http://www.appelsiini.net/'
 ));
@@ -44,9 +44,18 @@ if (class_exists('AutoLoader')) {
     Observer::observe('page_add_after_save',    'dashboard_log_page_add');
     Observer::observe('page_delete',            'dashboard_log_page_delete');
     
+    /* These currently only work in MIT fork of Frog. */
+    Observer::observe('layout_after_delete',    'dashboard_log_layout_delete');
+    Observer::observe('layout_after_add',       'dashboard_log_layout_add');
+    Observer::observe('layout_after_edit',      'dashboard_log_layout_edit');
+    
+    Observer::observe('snippet_after_delete',    'dashboard_log_snippet_delete');
+    Observer::observe('snippet_after_add',       'dashboard_log_snippet_add');
+    Observer::observe('snippet_after_edit',      'dashboard_log_snippet_edit');
+    
     function dashboard_log_page_add($page) {
         $linked_title     = sprintf('<a href="%s">%s</a>', 
-                                    '/admin/page/edit/' . $page->id, $page->title);
+                                    get_url('page/edit/' . $page->id), $page->title);
         $data['ident']    = 'core';
         $data['priority'] = DASHBOARD_LOG_NOTICE;
         $data['message']  = __('Page <b>:title</b> was created by :name', 
@@ -58,7 +67,7 @@ if (class_exists('AutoLoader')) {
 
     function dashboard_log_page_edit($page) {
         $linked_title     = sprintf('<a href="%s">%s</a>', 
-                                    '/admin/page/edit/' . $page->id, $page->title);
+                                    get_url('page/edit/' . $page->id), $page->title);
         $data['ident']    = 'core';
         $data['priority'] = DASHBOARD_LOG_NOTICE;
         $data['message']  = __('Page <b>:title</b> was revised by :name', 
@@ -73,6 +82,76 @@ if (class_exists('AutoLoader')) {
         $data['priority'] = DASHBOARD_LOG_NOTICE;
         $data['message']  = __('Page <b>:title</b> was deleted by :name', 
                                  array(':title' =>  $page->title, 
+                                       ':name' => AuthUser::getRecord()->name));
+        $entry = new DashboardLogEntry($data);
+        $entry->save();
+    }
+    
+    function dashboard_log_layout_delete($layout) {
+        $data['ident']    = 'core';
+        $data['priority'] = DASHBOARD_LOG_NOTICE;
+        $data['message']  = __('Layout <b>:title</b> was deleted by :name', 
+                                 array(':title' =>  $layout->name, 
+                                       ':name' => AuthUser::getRecord()->name));
+        $entry = new DashboardLogEntry($data);
+        $entry->save();
+    }
+
+    function dashboard_log_layout_add($layout) {
+        $linked_title     = sprintf('<a href="%s">%s</a>', 
+                                    get_url('layout/edit/' . $layout->id), $layout->name);
+        $data['ident']    = 'core';
+        $data['priority'] = DASHBOARD_LOG_NOTICE;
+        $data['message']  = __('Layout <b>:title</b> was created by :name', 
+                                 array(':title' =>  $linked_title, 
+                                       ':name' => AuthUser::getRecord()->name));
+        $entry = new DashboardLogEntry($data);
+        $entry->save();
+    }
+    
+    function dashboard_log_layout_edit($layout) {
+        $linked_title     = sprintf('<a href="%s">%s</a>', 
+                                    get_url('layout/edit/' . $layout->id), $layout->name);
+        $data['ident']    = 'core';
+        $data['priority'] = DASHBOARD_LOG_NOTICE;
+        $data['message']  = __('Layout <b>:title</b> was revised by :name', 
+                                 array(':title' =>  $linked_title, 
+                                       ':name' => AuthUser::getRecord()->name));
+        $entry = new DashboardLogEntry($data);
+        $entry->save();
+    }
+
+    /* Snippet */
+    
+    function dashboard_log_snippet_delete($snippet) {
+        $data['ident']    = 'core';
+        $data['priority'] = DASHBOARD_LOG_NOTICE;
+        $data['message']  = __('Snippet <b>:title</b> was deleted by :name', 
+                                 array(':title' =>  $snippet->name, 
+                                       ':name' => AuthUser::getRecord()->name));
+        $entry = new DashboardLogEntry($data);
+        $entry->save();
+    }
+
+    function dashboard_log_snippet_add($snippet) {
+        $linked_title     = sprintf('<a href="%s">%s</a>', 
+                                    get_url('snippet/edit/' . $snippet->id), $snippet->name);
+        $data['ident']    = 'core';
+        $data['priority'] = DASHBOARD_LOG_NOTICE;
+        $data['message']  = __('Snippet <b>:title</b> was created by :name', 
+                                 array(':title' =>  $linked_title, 
+                                       ':name' => AuthUser::getRecord()->name));
+        $entry = new DashboardLogEntry($data);
+        $entry->save();
+    }
+    
+    function dashboard_log_snippet_edit($snippet) {
+        $linked_title     = sprintf('<a href="%s">%s</a>', 
+                                    get_url('snippet/edit/' . $snippet->id), $snippet->name);
+        $data['ident']    = 'core';
+        $data['priority'] = DASHBOARD_LOG_NOTICE;
+        $data['message']  = __('Snippet <b>:title</b> was revised by :name', 
+                                 array(':title' =>  $linked_title, 
                                        ':name' => AuthUser::getRecord()->name));
         $entry = new DashboardLogEntry($data);
         $entry->save();
