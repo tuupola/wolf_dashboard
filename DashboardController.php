84282e4c4923346673f62cd86787fd68dfe474ac
Mika Tuupola <tuupola@appelsiini.net>
2009-03-12 17:57:41 +0200

    Fix problem were before yesterday actually showed all entries.

--- DashboardController.php
+++ DashboardController.php
@@ -29,7 +29,7 @@ class DashboardController extends PluginController
         $this->display('dashboard/views/index', array(
             'log_entry_today' => Record::findAllFrom('DashboardLogEntry', 'created_on > CURRENT_DATE() ORDER BY created_on DESC'),
             'log_entry_yesterday' => Record::findAllFrom('DashboardLogEntry', 'created_on > DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) AND created_on < CURRENT_DATE() ORDER BY created_on DESC'),
-            'log_entry_older' => Record::findAllFrom('DashboardLogEntry', '1=1 ORDER BY created_on DESC')
+            'log_entry_older' => Record::findAllFrom('DashboardLogEntry', 'created_on < DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) ORDER BY created_on DESC')
         ));
     }
     
