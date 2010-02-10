db89276087a322bd675f0dc672df58a0eba23dd4
Mika Tuupola <tuupola@appelsiini.net>
2010-02-10 17:19:51 +0200

    Adjust image paths for Wolf.

--- views/index.php
+++ views/index.php
@@ -1,12 +1,13 @@
-<h1><?php echo __('Dashboard'); ?></h1>
-<?php error_reporting(E_ALL); ?>
+<p>
+    <h1><?php echo __('Dashboard'); ?></h1>
+</p>
 
 <form action="<?php echo get_url('plugin/dashboard/clear'); ?>" method="post">
   <b><?php echo __('Today'); ?></b>
   <table class="fieldset" cellpadding="0" cellspacing="0" border="0">
   <?php foreach ($log_entry_today as $entry): ?> 
     <tr class="<?php print odd_even(); ?>">
-      <td class="priority"><img src="../frog/plugins/dashboard/img/<?php print $entry->priority('string') ?>.png" title="<?php print $entry->priority('string') ?>" /></td>
+      <td class="priority"><img src="../wolf/plugins/dashboard/img/<?php print $entry->priority('string') ?>.png" title="<?php print $entry->priority('string') ?>" /></td>
       <td><?php print $entry->message ?></td>
       <td class="date"><a title="<?php print $entry->created_on ?>"><?php print DateDifference::getString(new DateTime($entry->created_on)); ?></a></td>
     </tr>	
@@ -18,7 +19,7 @@
   <table class="fieldset" cellpadding="0" cellspacing="0" border="0">
   <?php foreach ($log_entry_yesterday as $entry): ?>
     <tr class="<?php print odd_even(); ?>">
-      <td class="priority"><img src="../frog/plugins/dashboard/img/<?php print $entry->priority('string') ?>.png" title="<?php print $entry->priority('string') ?>" /></td> 
+      <td class="priority"><img src="../wolf/plugins/dashboard/img/<?php print $entry->priority('string') ?>.png" title="<?php print $entry->priority('string') ?>" /></td> 
       <td><?php print $entry->message ?></td>
       <td class="date"><a title="<?php print $entry->created_on ?>"><?php print DateDifference::getString(new DateTime($entry->created_on)); ?></a></td>
     </tr>	
@@ -30,7 +31,7 @@
   <table class="fieldset" cellpadding="0" cellspacing="0" border="0">
   <?php foreach ($log_entry_older as $entry): ?>
     <tr class="<?php print odd_even(); ?>">
-      <td class="priority"><img src="../frog/plugins/dashboard/img/<?php print $entry->priority('string') ?>.png" title="<?php print $entry->priority('string') ?>" /></td>
+      <td class="priority"><img src="../wolf/plugins/dashboard/img/<?php print $entry->priority('string') ?>.png" title="<?php print $entry->priority('string') ?>" /></td>
       <td><?php print $entry->message ?></td>
       <td class="date"><a title="<?php print $entry->created_on ?>"><?php print DateDifference::getString(new DateTime($entry->created_on)); ?></a></td>
     </tr>	
